59724652989140a9c690ea1631f2b61e2b6c370a
DESKTOP-13AFVKA\abachkay <abachkay@gmail.com>
2017-08-19 17:31:42 +0300

    Project files added. Gitignore changed.

---
+++ oleksandrbachkai/oleksandrbachkai/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using oleksandrbachkai.App_Start;
+
+
+[assembly: OwinStartup(typeof(oleksandrbachkai.Startup))]
+
+namespace oleksandrbachkai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
