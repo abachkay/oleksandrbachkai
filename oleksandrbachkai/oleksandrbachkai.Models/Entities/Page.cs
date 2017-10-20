1ec024e06d1b8082bce6303153c0ef8acd1da79e
DESKTOP-13AFVKA\abachkay <abachkay@gmail.com>
2017-10-20 20:52:23 +0300

    DbContest and entities added.

---
+++ oleksandrbachkai/oleksandrbachkai.Models/Entities/Page.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace oleksandrbachkai.Models
+{
+    public class Page
+    {
+        [Key]
+        public int PageId { get; set; }        
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
