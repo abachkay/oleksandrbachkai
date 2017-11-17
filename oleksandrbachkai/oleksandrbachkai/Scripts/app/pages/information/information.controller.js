5770588ecaf8be4d25ab523b75a3e0541618ece8
Oleksandr Arkhipov <oleksandr_arkhipov@epam.com>
2017-11-17 15:17:04 +0200

    develop: tinymce configured for information page.

--- oleksandrbachkai/oleksandrbachkai/Scripts/app/pages/information/information.controller.js
+++ oleksandrbachkai/oleksandrbachkai/Scripts/app/pages/information/information.controller.js
@@ -6,5 +6,29 @@
     function linksController($scope, $cookies) {
         var vm = this;
         vm.title = 'info';
+
+        // tinymce options
+        vm.tinymceModel = 'sdfg';
+        vm.tinymceOptions = {
+            setup: function (editor) {
+                editor.on("init", function () {
+
+                });
+            },
+            branding: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+                'emoticons template paste textcolor colorpicker textpattern imagetools'
+            ],
+            toolbar1: 'insertfile undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons',
+            image_advtab: true,
+            templates: [
+                { title: 'Test template 1', content: 'Test 1' },
+                { title: 'Test template 2', content: 'Test 2' }
+            ],
+
+        };
     }
 })(angular);
