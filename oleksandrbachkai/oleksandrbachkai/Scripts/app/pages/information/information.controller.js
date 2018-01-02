cf94e00d5fd2c598dd52e8e9dba68ad70efc167f
Oleksandr Bachkai <abachkay@gmail.com>
2018-01-02 18:29:47 +0200

    information_management: set default visibility of visual blocks in editor.

--- oleksandrbachkai/oleksandrbachkai/Scripts/app/pages/information/information.controller.js
+++ oleksandrbachkai/oleksandrbachkai/Scripts/app/pages/information/information.controller.js
@@ -59,6 +59,7 @@
             ],
             toolbar1: 'insertfile undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media | forecolor backcolor emoticons | drive',
             image_advtab: true,
+            visualblocks_default_state: true
         };
     }
 })(angular);
