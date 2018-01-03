b4ef6d0ed08ead7ef9150875efdbc85edb2067c9
Oleksandr Bachkai <abachkay@gmail.com>
2018-01-03 13:36:02 +0200

    information_management: added google drive adapter, implemented get link by file id and get all files.

---
+++ oleksandrbachkai/oleksandrbachkai.Adapters/GoogleDriveAdapter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using File = Google.Apis.Drive.v3.Data.File;
+
+namespace oleksandrbachkai.Adapters
+{
+    public class GoogleDriveAdapter
+    {
+        private readonly DriveService _service;
+
+        public GoogleDriveAdapter()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = AppDomain.CurrentDomain.BaseDirectory;
+                credPath = Path.Combine(credPath, "drive-dotnet-quickstart.json");
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[]
+                    {
+                        DriveService.Scope.DriveReadonly
+                    },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            _service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+            });
+        }
+
+        public IEnumerable<File> GetAllFiles()
+        {
+            
+            var listRequest = _service.Files.List();
+
+            listRequest.Fields = "nextPageToken, files";
+
+            return  listRequest.Execute().Files;            
+        }
+
+        public static string GetUrlByFileId(string fileId)
+        {
+            return $"https://docs.google.com/document/d/{fileId}/edit?usp=sharing";            
+        }
+    }
+}
---
+++ oleksandrbachkai/oleksandrbachkai.Adapters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace oleksandrbachkai.Adapters
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine(GoogleDriveAdapter.GetUrlByFileId("1MkjG02EKWJViu_IQidEnGGHZCs7pXWxIMg0Fdslehms"));
+
+            foreach (var file in new GoogleDriveAdapter().GetAllFiles())
+            {
+                Console.WriteLine(file.WebViewLink);
+            }
+
+            Console.Read();
+        }
+    }
+}
